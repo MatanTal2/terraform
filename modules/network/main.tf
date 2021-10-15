3ca961dfcace8cdd1b37d46b530d71fd4a861a78
Matan Tal <matantal2@gmail.com>
2021-10-15 13:16:33 +0300

    edit readme

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -101,3 +101,16 @@ resource "azurerm_network_interface" "web_server" {
     private_ip_address_allocation = "dynamic"
   }
 }
+
+# =============== Load balancer ===============
+resource "azurerm_lb" "example" {
+  name                = "TestLoadBalancer"
+  location            = var.cloud_location
+  resource_group_name = var.rg_name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.to_front_lb.id
+    
+  }
+}
