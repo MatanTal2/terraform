c96e1aeeeb4866755de3a8878805c9398af8d9bd
Matan Tal <matantal2@gmail.com>
2021-10-17 15:23:42 +0300

    create bonus directory app is runnig

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "web_admin_pass" {
-  value = module.virtual_machines.admin_pass
+  value = module.virtual_machines[*].admin_pass
 }
 output "database_admin_pass" {
   value = module.database.admin_pass
